d146636453204003409e7c49e6e61245bbf59dd3
Gulshan <73947033+gulshan-18@users.noreply.github.com>
2021-07-05 13:44:13 +0530

    Add files via upload

---
+++ result.js
@@ -0,0 +1,7 @@
+res=document.getElementById("res")
+alert("res")
+        if(res==1){
+            document.getElementById("res").innerHTML="Spam"
+        }else{
+            document.getElementById("res").innerHTML="Non-Spam"
+        }
