b9a5cd3ca9a8f51f67cd394044e70560bf4e7367
Gulshan <73947033+gulshan-18@users.noreply.github.com>
2021-08-08 11:20:37 +0530

    Delete result.js

--- result.js
+++
@@ -1,7 +0,0 @@
-res=document.getElementById("res")
-alert("res")
-        if(res==1){
-            document.getElementById("res").innerHTML="Spam"
-        }else{
-            document.getElementById("res").innerHTML="Non-Spam"
-        }
